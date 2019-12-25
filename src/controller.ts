9b375200f87b65def3d57277e82b87c9b615eaf5
Jeremias Aagaard <jereaa2@gmail.com>
2019-12-25 00:51:52 -0300

    Output retrieved value to file

--- src/controller.ts
+++ src/controller.ts
@@ -1,3 +1,5 @@
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 import { CrawlerCallback } from 'crawler';
 
 const callback: CrawlerCallback = (error, res, done) => {
@@ -11,7 +13,17 @@ const callback: CrawlerCallback = (error, res, done) => {
     return console.error("Coudln't get selector to select from the retrieved HTML");
   }
 
-  console.log($('.entry-content').html());
+  let text = $('.entry-content > p').eq(2).html();
+
+  if (!text) {
+    console.error('Couldn\'t retrieve any HTML from selection.');
+    return process.exit(1);
+  }
+
+  text = text.replace(/&#x3000;/g, '<p>');
+  text = text.replace(/<br>/g, '</p>');
+
+  writeFileSync(join(__dirname, '/../../', '/output.xhtml'), text);
   return done();
 };
 
